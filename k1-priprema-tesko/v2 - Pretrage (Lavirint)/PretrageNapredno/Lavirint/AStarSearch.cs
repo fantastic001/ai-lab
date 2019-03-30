9efe9258fb3ebdcfc06b09aaa52116192365d126
Stefan Nozinic (uib56677) <stefan.nozinic@continental-corporation.com>
2019-03-30 11:56:58 +0100

    K1 priprema tezak zadatak pokupiti 2 kutije sto dalje od vatre

---
+++ k1-priprema-tesko/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/AStarSearch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Lavirint
+{
+    class AStarSearch
+    {
+        private State findMin(List<State> elements) {
+            State res = elements[0];
+            foreach (State elem in elements) {
+                if (this.heuristicFunction(res) > this.heuristicFunction(elem)) {
+                    res = elem;
+                }
+            }
+            return res;
+        }
+        public State search(State pocetnoStanje)
+        {
+            // TODO 5.1: Implementirati algoritam vodjene pretrage A*
+            List<State> stanjaNaObradi = new List<State>();
+            Hashtable visited = new Hashtable();
+            stanjaNaObradi.Add(pocetnoStanje);
+            while (stanjaNaObradi.Count > 0)
+            {
+                State naObradi = findMin(stanjaNaObradi);
+
+                if (! visited.ContainsKey(naObradi.GetHashCode()))
+                {
+                    Main.allSearchStates.Add(naObradi);
+                    visited.Add(naObradi.GetHashCode(), null);
+                    naObradi.PrintPath();
+                    if (naObradi.isKrajnjeStanje())
+                    {
+                        return naObradi;
+                    }
+                    List<State> mogucaSledecaStanja = naObradi.mogucaSledecaStanja();
+                    stanjaNaObradi.AddRange(mogucaSledecaStanja);
+                }
+                stanjaNaObradi.Remove(naObradi);
+            }
+            return null;
+        }
+
+        
+        public double heuristicFunction(State s)
+        {
+            // TODO 5.2: Implementirati heuristicku funkciju (funkcija odredjuje rastojanje)
+            int fireI=0, fireJ=0;
+            for (int i = 0; i < Main.lavirint.brojVrsta; i++) {
+                for (int j = 0; j < Main.lavirint.brojKolona; j++) {
+                    if (Main.lavirint.polja[i, j] == 5) {
+                        fireI = i;
+                        fireJ = j; 
+                    }
+                }
+            }
+            double c = s.cost;
+            return s.cost + Math.Abs(Main.krajnjiNode.markI - s.node.markI)
+                //+ Math.Abs(Main.krajnjiNode.markJ - s.node.markJ)
+                - 10*Math.Abs(fireI - s.node.markI)  - 10*Math.Abs(fireJ - s.node.markJ)
+                ;
+        }
+    }
+}
---
+++ k1-priprema-tesko/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/State.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lavirint
+{
+    public class State
+    {
+        public State parent;
+        public Node node;
+        public double cost;
+        public int level;
+        public List<Node> kutije = new List<Node>();
+
+        public State sledeceStanje(Node node)
+        {
+            State rez = new State();
+            rez.node = node;
+            rez.parent = this;
+            rez.cost = this.cost + 1;
+            rez.level = this.level + 1;
+            rez.kutije = new List<Node>(kutije);
+            if (Main.lavirint.polja[node.markI, node.markJ] == 4) {
+                if (! rez.kutije.Contains(node)) rez.kutije.Add(node);
+            }
+            return rez;
+        }
+
+
+        public List<State> mogucaSledecaStanja()
+        {
+            List<State> rez = new List<State>();
+            foreach (Node nextNode in this.node.getLinkedNodes())
+            {
+                State next = sledeceStanje(nextNode);
+                rez.Add(next);
+            }
+            return rez;
+        }
+
+        public bool isKrajnjeStanje()
+        {
+            return Main.krajnjiNode.Equals(this.node) && kutije.Count == 2;
+        }
+
+        public List<State> path()
+        {
+            List<State> putanja = new List<State>();
+            State tt = this;
+            while (tt != null)
+            {
+                putanja.Insert(0, tt);
+                tt = tt.parent;
+            }
+            return putanja;
+        }
+
+        public bool cirkularnaPutanja()
+        {
+            // TODO 3: proveriti da li trenutno stanje odgovara poziciji koja je vec vidjena u grani pretrazivanja
+            List<State> grana = this.path();
+            int i;
+            int c = 0;
+            for (i = 0; i < grana.Count; i++) {
+                if (grana[i].Equals(this)) c++;
+            }
+            return c > 1;        
+        }
+
+        public override int GetHashCode()
+        {
+            int code =  100 * 100 * this.node.markI + 100 * node.markJ;
+            for (int i = 0; i < 2; i++) {
+                code *= 10000;
+                if (i < kutije.Count)
+                {
+                    code += node.GetHashCode();
+                }
+                else {
+                    code += 9999;
+                }
+            }
+            return code; 
+        }
+
+        public override bool Equals(object obj)
+        {
+            State other = (State)obj;
+            return other.node.markI == node.markI && other.node.markJ == node.markJ && 
+                other.kutije.Count == kutije.Count;
+        }
+        public void PrintPath() {
+            List<State> p = path();
+            foreach (State s in p) {
+                Console.Write(s.node.markI + "-" + s.node.markJ + "-" + s.kutije.Count);
+                Console.Write(" -> ");
+            }
+            Console.WriteLine();
+        }
+    }
+   
+}
